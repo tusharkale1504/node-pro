27fbd1317e757aad54584857d18f550e237ca98f
Tushar Sanjay Kale <kaletushar2003@gmail.com>
2024-10-22 14:50:14 +0530

    shift mongodb to online server

--- Routes/personRoutes.js
+++ Routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('../models/person');
+const Person = require('../models/Person');
 
 //Post route  to add a person.
 
